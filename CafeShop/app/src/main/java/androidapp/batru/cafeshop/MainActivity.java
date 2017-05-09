ebc0643e10ee829f0313a943272e23f5ec9a1b32
BaTru <gietbarca@gmail.com>
2017-05-09 16:50:52 +0700

    Hoan thanh toolbar

--- CafeShop/app/src/main/java/androidapp/batru/cafeshop/MainActivity.java
+++ CafeShop/app/src/main/java/androidapp/batru/cafeshop/MainActivity.java
@@ -2,6 +2,10 @@ package androidapp.batru.cafeshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Ban hang");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_add) {
+            Toast.makeText(this, "CLick here", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
